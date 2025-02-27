5db049cb0967f944ce3cfc835340b3068c2f155f
Nicolas <nicolasluque0013@gmail.com>
2025-02-27 11:09:10 -0300

    fix

--- App NodeJS/app.js
+++ App NodeJS/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3001;
 
 // Esta función se ejecutará cuando alguien visite la URL raíz
 app.get('/', (req, res) => {
