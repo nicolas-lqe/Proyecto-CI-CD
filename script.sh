0d7806b0670288f7e4aa89b1bf11ed8ff153e81e
nicolas-lqe <nicolasluque0013@gmail.com>
2025-03-05 20:08:20 -0300

    ...

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ apt-get update
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
-DOCKER_COMPOSE_VERSION=1.29.2  # Cambia a la versión deseada
+DOCKER_COMPOSE_VERSION=1.29.2  
 curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
